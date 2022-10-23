40c4f3f04a478a359e6130f097b4aa059c9b2189
Jorge Emiliano Maldonado <jorgeemilianom@gmail.com>
2022-10-22 22:40:53 -0300

    Innit project

---
+++ components/Module.jsx
@@ -0,0 +1,15 @@
+// React dependecies
+const { useState, useEffect, createContext, useContext, useRef } = React;
+
+const App = () => {
+
+    return (
+        <h1>hola</h1>
+    )
+}
+
+
+
+// Rendering
+const module = document.querySelector('#module');
+ReactDOM.render(<App />, module);
